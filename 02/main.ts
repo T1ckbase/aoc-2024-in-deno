25851bf6c99a13ecc337c312c0328519fca4dfae
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2024-12-03 20:07:27 +0800

    .

--- 02/main.ts
+++ 02/main.ts
@@ -8,13 +8,10 @@ function isSafe(report: number[]): boolean {
 const input = Deno.readTextFileSync('./input.txt');
 
 const lines = input.trim().split(/\r\n|\n|\r/);
-
 const reports = lines.map(line => line.split(/\s+/).map(Number));
 
 const safeReports = reports.filter(isSafe);
-
 console.log('part1', safeReports.length);
 
 const safeReports2 = reports.filter(report => Array.from({length: report.length}, (_, i) => report.toSpliced(i, 1)).some(isSafe));
-
 console.log('part2', safeReports2.length);
